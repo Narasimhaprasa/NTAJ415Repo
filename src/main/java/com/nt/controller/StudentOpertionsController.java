0f98402e0d61f45126c9407d1f299e2fa451d631
Lakshmi Narasimhaprasad <92583589+Narasimhaprasa@users.noreply.github.com>
2023-08-02 19:50:55 +0530

    Update StudentOpertionsController.java
    Changes from Remote Repository

--- src/main/java/com/nt/controller/StudentOpertionsController.java
+++ src/main/java/com/nt/controller/StudentOpertionsController.java
@@ -5,6 +5,7 @@ package com.nt.controller;
  * Data : August 2 2023
  * Branch : CSE BTECH
  * College : Yogananda Institute Of Technology & Science
+ * Localtion : Tiruapathi
  */
 import java.util.List;
 import java.util.Map;
